012fad1a32d7e50487910f6ca81ab809f035ea30
aleheh201 <aleheh201@studentmail.oru.se>
2022-05-10 11:34:15 +0200

    reconstruction

---
+++ src/main/java/Discipline/Station.java
@@ -0,0 +1,13 @@
+package Discipline;
+
+public enum Station {
+    RUNNING_TRACK,
+    SPRINT_TRACK,
+    LONG_TRIPLE_I,
+    LONG_TRIPLE_II,
+    HIGH_JUMP_I,
+    HIGH_JUMP_II,
+    POLE_VAULT,
+    SHOT_PUT_I,
+    SHOT_PUT_II
+}
