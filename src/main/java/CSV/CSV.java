0783d6f2078aa1b5f22113582b2f2c5707d7a824
= <aleheh201@studentmail.oru.se>
2022-05-14 22:45:33 +0200

    refactor(CSV): Rename, Athlete ID
    Renamed to CSV, athlete ID now starts at 1

--- src/main/java/CSV/CSV.java
+++ src/main/java/CSV/CSV.java
@@ -10,7 +10,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SCIA_CSV {
+public class CSV {
     private static final String DELIMITER = ",";
     private static final int NUM_PROPERTIES = Property.values().length - 1;
 
@@ -22,13 +22,13 @@ public class SCIA_CSV {
             List<String> columns = new ArrayList<>(List.of(rows.get(i).split(DELIMITER)));
             List<String> athlete_columns = columns.subList(0, NUM_PROPERTIES);
             List<String> discipline_records_columns = columns.subList(NUM_PROPERTIES, columns.size());
-            Athlete athlete = SCIA_RecordReader.getAthlete(i, athlete_columns);
-            DisciplineRecords discipline_records = SCIA_RecordReader.getDisciplineRecords(discipline_records_columns);
+            Athlete athlete = RecordReader.getAthlete(i+1, athlete_columns);
+            DisciplineRecords discipline_records = RecordReader.getDisciplineRecords(discipline_records_columns);
             this.records.add(new AthleteRecord(athlete, discipline_records));
         }
     }
 
-    public SCIA_CSV(String csvFilePath) throws IOException {
+    public CSV(String csvFilePath) throws IOException {
         this.csvFilePath = csvFilePath;
         this.records = new ArrayList<>();
         this.readRecords();
