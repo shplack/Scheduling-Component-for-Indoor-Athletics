c70740b9a0eeb28d750ffab62e5b0c61fa2453aa
malli <mallismallalieu2@gmail.com>
2022-05-16 11:11:24 +0200

    test(testing): Event Test
    Updating branch

--- src/test/java/SimulatedAnneling/JudgementTest.java
+++ src/test/java/SimulatedAnneling/JudgementTest.java
@@ -5,6 +5,7 @@ import Discipline.Discipline;
 import Discipline.Station;
 import Schedule.Event;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,55 +14,79 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JudgementTest {
+    ArrayList<Event> eventList;
 
-    Event event;
-    Event event_2;
-    Event event_3;
     @BeforeEach
     void setUp() {
-        event = new Event(
+        Event event = new Event(
                 new int[]{1, 2, 3},
-                (ArrayList<Athlete>) Arrays.asList(new Athlete(
+                new ArrayList<>(Arrays.asList(new Athlete(
                         1,
                         "ÖSK",
                         "Alexander",
                         "Hedberg",
                         Gender.MALE,
                         29
-                )),
+                ))),
                 Station.RUNNING_TRACK,
                 Discipline.LONG3000M
         );
 
-        event_2 = new Event(
+        Event event_2 = new Event(
                 new int[]{4, 5, 6},
-                (ArrayList<Athlete>) Arrays.asList(new Athlete(
+                new ArrayList<>(Arrays.asList(new Athlete(
                         2,
                         "ÖÖÖ",
                         "Victor",
                         "Jonsson",
                         Gender.MALE,
                         30
-                )),
+                ))),
                 Station.LONG_TRIPLE_I,
                 Discipline.TRIPLE_JUMP
         );
 
-        event_3 = new Event(
+        Event event_3 = new Event(
                 new int[]{2},
-                (ArrayList<Athlete>) Arrays.asList(new Athlete(
+                new ArrayList<>(Arrays.asList(new Athlete(
                         1,
                         "ÖSK",
                         "Alexander",
                         "Hedberg",
                         Gender.MALE,
                         29
-                )),
+                ))),
                 Station.POLE_VAULT,
                 Discipline.POLE_VAULT
         );
-
-        ArrayList<Event> eventLista  = new ArrayList<Event>(List.of(event,event_2,event_3));
+        eventList  = new ArrayList<>(List.of(event,event_2,event_3));
     }
+
+
+
+    @Test
+    void getTimeSlot() {
+        for (Event event : eventList)
+            for (int k = 0; k < event.time_slots().length; k++)
+                System.out.println(event.time_slots()[k]);
+        //loops through list and compares each timeslot of the event
+        //with the timeslot of the other events and checks if it is the same athlete
+        //in two timeslots
+        /*
+        for (int i = 0; i < eventList.size(); i++) {
+            for (int j = 0; j < eventList.size(); j++) {
+                for (int k = 0; k < eventList.get(i).time_slots().length; k++)
+                    for (int l = 0; l < eventList.get(j).time_slots().length; l++)
+                        eventList.get(i).time_slots(k).equals(eventList.get(j).time_slots(l));
+            }
+        }
+
+         */
+    }
+
+
+
+
+
 
 }
